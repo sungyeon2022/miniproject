d9f19837c10669106fc3b7e217e139328700b685
jo_sung <zounsam@naver.com>
2023-09-05 10:33:45 +0900

    Error Fix

--- minigame/src/main/miniApp.java
+++ minigame/src/main/miniApp.java
@@ -4,7 +4,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
 import java.util.Vector;
 
-
 import javax.swing.JFrame;
 
 import map.Background;
@@ -13,12 +12,12 @@ import player.issac;
 //JFrame 참조 
 public class miniApp extends JFrame {
 	private JFrame app;
-	
+
 	private Background bg;
 	private issac issac;
-	
-	//miniApp에서 필요한 시스템 정보 가져옴
-	public miniApp(){
+
+	// miniApp에서 필요한 시스템 정보 가져옴
+	public miniApp() {
 		init();
 		setting();
 		batch();
@@ -26,106 +25,83 @@ public class miniApp extends JFrame {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				
-				
+
 			}
 		}).start();
 		setVisible(true);
 	}
-	
-	//앱에서 필요한 데이터정보 가져옴
+
+	// 앱에서 필요한 데이터정보 가져옴
 	public void init() {
-		app=this;
+		app = this;
 		bg = new Background(app);
 		issac = new issac(app);
-		
+
 	}
-	
-	//JFrame을 통한 창출력
+
+	// JFrame을 통한 창출력
 	public void setting() {
 		app.setTitle("miniApp");
-		app.setSize(960,640);
+		app.setSize(960, 640);
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		app.setLocationRelativeTo(null);
 		app.setLayout(null);
 	}
+
 	public void batch() {
 	}
+
 	public void listener() {
 		keyboardEvent();
 	}
-	
+
 	public static void main(String[] args) {
 		new miniApp();
 	}
-	
+
 	public void keyboardEvent() {
 		addKeyListener(new KeyAdapter() {
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 					issac.moveRight();
-			}
-			if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 					issac.moveLeft();
-			}
-			if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 					issac.moveDown();
-			}
-			if(e.getKeyCode()==KeyEvent.VK_UP) {
+				} else if (e.getKeyCode() == KeyEvent.VK_UP) {
 					issac.moveUp();
+				} else if (e.getKeyCode() == KeyEvent.VK_W) {
+
+				} else if (e.getKeyCode() == KeyEvent.VK_D) {
+
+				} else if (e.getKeyCode() == KeyEvent.VK_A) {
+
+				} else if (e.getKeyCode() == KeyEvent.VK_S) {
+
+				} else if (e.getKeyCode() == KeyEvent.VK_E) {
+
+				}
 			}
-			if(e.getKeyCode()==KeyEvent.VK_W) {
-				
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					issac.setRight(false);
+					issac.refreshDirect();
+				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					issac.setLeft(false);
+					issac.refreshDirect();
+				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					issac.setDown(false);
+					issac.refreshDirect();
+				} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+					issac.setUp(false);
+					issac.refreshDirect();
+				}
+
 			}
-			if(e.getKeyCode()==KeyEvent.VK_D) {
-				
-			}
-			if(e.getKeyCode()==KeyEvent.VK_A) {
-				
-			}
-			if(e.getKeyCode()==KeyEvent.VK_S) {
-				
-			}
-			if(e.getKeyCode()==KeyEvent.VK_E) {
-				issac.moveRight();
-			}else if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-				issac.moveLeft();
-			}else if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-				issac.moveDown();
-			}else if(e.getKeyCode()==KeyEvent.VK_UP) {
-				issac.moveUp();
-			}else if(e.getKeyCode()==KeyEvent.VK_W) {
-				
-			}else if(e.getKeyCode()==KeyEvent.VK_D) {
-				
-			}else if(e.getKeyCode()==KeyEvent.VK_A) {
-				
-			}else if(e.getKeyCode()==KeyEvent.VK_S) {
-				
-			}else if(e.getKeyCode()==KeyEvent.VK_E) {
-				
-			}
-		}
-		@Override
-		public void keyReleased(KeyEvent e) {
-			if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-				issac.setRight(false);
-				issac.refreshDirect();
-			}else if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-				issac.setLeft(false);
-				issac.refreshDirect();
-			}else if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-				issac.setDown(false);
-				issac.refreshDirect();
-			}else if(e.getKeyCode()==KeyEvent.VK_UP) {
-				issac.setUp(false);
-				issac.refreshDirect();
-			}
-			
-		}
 		});
 	}
 
 }
-
