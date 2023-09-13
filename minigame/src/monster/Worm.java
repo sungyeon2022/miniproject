831f6d9b3f2d3c60a6f866106be5f611ac695606
KangWookLim <lakumwa99@gmail.com>
2023-09-13 16:50:42 +0900

    몬스터 버그 픽스 / 진행중/

--- minigame/src/monster/Worm.java
+++ minigame/src/monster/Worm.java
@@ -6,6 +6,7 @@ import java.util.TimerTask;
 import javax.swing.JFrame;
 
 import SpriteSheet.SpriteSheet;
+import imgSize.Gap;
 import imgSize.ViewDirect;
 import imgSize.WormSize;
 import lombok.Data;
@@ -14,7 +15,7 @@ import player.issac;
 @Data
 
 public class Worm extends Monster {
-	private boolean monsterAttacking = false;
+	public static boolean monsterAttacking = false;
 	private static int gapX ;
 	private static int gapY;
 	private boolean checkAttackDirectX = false;
@@ -31,12 +32,6 @@ public class Worm extends Monster {
 				TimerTask task = new TimerTask() { //4초마다 방향전환
 					public void run() {
 						if(monsterAttacking == false) {
-//							System.out.println("아이작 X" + getIssac().getXPlayer());
-//							System.out.println("벌레 X" + getXPlayer());
-//							System.out.println("아이작 Y" + getIssac().getYPlayer());
-//							System.out.println("벌레 Y" + getYPlayer());
-//							System.out.println(gapX);
-//							System.out.println(gapY);
 							System.out.println("방향전환");
 							moveDirectCheck();	
 						}
@@ -46,7 +41,6 @@ public class Worm extends Monster {
 				long period = 4000L;
 				timer.schedule(task, delay, period);
 				while(!isDead()) {
-					System.out.print(".");
 					if(getLife() <= 0) {
 						setDead(true);
 						break;
@@ -58,8 +52,12 @@ public class Worm extends Monster {
 					moveDown();
 					moveRight();
 					moveLeft();
-					moveMotion();
+					System.out.println(monsterAttacking);
+					if(monsterAttacking == false)
+					{
+						moveMotion();
 					getSsMonster().drawObj(getXPlayer(), getYPlayer());
+					}
 						try {
 						Thread.sleep(30);
 					} catch (InterruptedException e) {
@@ -103,59 +101,195 @@ public class Worm extends Monster {
 	public void attack() { //아이작과 몬스터 겹치면 공격 시작
 
 		if (Math.abs(gapX) < 10 || Math.abs(gapY) < 10 ) {
-			if(gapX < 10 ) {
+			if(Math.abs(gapX) < 10 ) {
 				checkAttackDirectY = true;
 			}else {
+
 				checkAttackDirectX = true;
 			}
 			attackOn();
 		}
 	}
+	public void attackMotion(int direct) {
+//		getSsMonster().setXPos((WormSize.WIDTH * direct) + (Gap.COLUMGAP * direct));
+//		getSsMonster().setYPos(WormSize.HEIGHT * 4 + Gap.ROWGAP * 4);
+//		getSsMonster().drawObj(getXPlayer(), getYPlayer());
+//		
+//
+//		getSsMonster().setXPos(0);
+//		getSsMonster().setYPos(WormSize.HEIGHT * direct + Gap.ROWGAP * direct);
+//		getSsMonster().drawObj(getXPlayer(), getYPlayer());
+//		
+//		
+//		getIssac().setLife(getIssac().getLife() - 1);	// 플레이어 생명력 1감소
+//		getIssac().dead();
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+	}
 	public void attackOn() {
-		System.out.println("공격");
+//		System.out.println(getLife());
+//		System.out.println(isDead());
+//		System.out.println("공격");
+		monsterSpeed = 4;
+		monsterAttacking = true;
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		while(getXPlayer() > 130 && getXPlayer() < 790 && getYPlayer() > 100 && getYPlayer() <440) {
-			monsterAttacking = true;
-			monsterSpeed = 4;
+			
 			if( checkAttackDirectX && (gapX > 0)) { //오른쪽 공격
-//				System.out.println("오른쪽");
 				setViewDirect(ViewDirect.RIGHT);
-//				moveRight();
+				attackMotion(ViewDirect.RIGHT);
+				moveRight();
 				setLeft(false);
 				setRight(true);
 				setDown(false);
 				setUp(false);
 			}else if ( checkAttackDirectX && (gapX <= 0)) { //왼쪽 공격
-//				System.out.println("왼쪽");
 				setViewDirect(ViewDirect.LEFT);
-//				moveLeft();
+				attackMotion(ViewDirect.LEFT);
+				moveLeft();
 				setRight(false);
 				setLeft(true);
 				setDown(false);
 				setUp(false);
 			}else if ( checkAttackDirectY && (gapY > 0)) {//아래로 공격
-//				System.out.println("아래쪽");
 				setViewDirect(ViewDirect.DOWN);
-//				moveDown();
+				attackMotion(ViewDirect.DOWN);
+				moveDown();
 				setLeft(false);
 				setRight(false);
 				setUp(false);
 				setDown(true);
 			}else if ( checkAttackDirectY && (gapY <= 0)) { //위로 공격
-//				System.out.println("위쪽");
 				setViewDirect(ViewDirect.UP);
-//				moveUp();
+				attackMotion(ViewDirect.UP);
+				moveUp();
 				setLeft(false);
 				setRight(false);
 				setDown(false);
 				setUp(true);
 			}
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			
 //			attackMotion();
 			
 		}
+		monsterAttacking = false;
+		checkAttackDirectY = false;
+		checkAttackDirectX = false;
+		monsterSpeed = 2;
 		
 	
 		
 	}
+
+	@Override
+	public void moveRight() {
+		if(isRight()) {
+			setViewDirect(ViewDirect.RIGHT);
+				setXPlayer(getXPlayer() + monsterSpeed);
+				setXPlayerCenter(getXPlayerCenter() + monsterSpeed);
+		}
+	}
+	@Override
+	public void moveLeft() {
+		if(isLeft()) {
+			setViewDirect(ViewDirect.LEFT);
+				setXPlayer(getXPlayer() - monsterSpeed);
+				setXPlayerCenter(getXPlayerCenter() - monsterSpeed);
+			
+		}
+	}
+	@Override
+	public void moveUp() {
+		if(isUp()) {
+			setViewDirect(ViewDirect.UP);
+				setYPlayer(getYPlayer() - monsterSpeed);
+				setYPlayerCenter(getYPlayerCenter() - monsterSpeed);
+			
+		}
+		
+	}
+	@Override
+	public void moveDown() {
+		if(isDown()) {
+			setViewDirect(ViewDirect.DOWN);
+				setYPlayer(getYPlayer() + monsterSpeed);
+				setYPlayerCenter(getYPlayerCenter() + monsterSpeed);
+			
+		}
+	}
+	public void moveMotion() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int motion = 0;
+				if(isPlayerMoveStart() == false ) {
+					setPlayerMoveStart(true);
+					while(!isDead()) {
+						if(isDown() && getViewDirect() == ViewDirect.DOWN) {
+							if(motion > 3)
+								motion = 0;
+							getSsMonster().setXPos((getImgWidth() * motion) + (Gap.COLUMGAP * motion));
+							if(getViewDirect() == ViewDirect.DOWN) {
+								getSsMonster().setYPos(getImgHeight() * 0 + Gap.ROWGAP * 0);	// 몸 이미지 y좌표
+								getSsMonster().drawObj(getXPlayer(), getYPlayer());
+								motion += 1;
+							}
+						}
+						else if(isLeft() && getViewDirect() == ViewDirect.LEFT) {
+							if(motion > 3)
+								motion = 0;
+							getSsMonster().setXPos((getImgWidth() * motion) + (Gap.COLUMGAP * motion));
+							if(getViewDirect() == ViewDirect.LEFT) {
+								getSsMonster().setYPos(getImgHeight() * 1 + Gap.ROWGAP * 1);	// 몸 이미지 y좌표
+								getSsMonster().drawObj(getXPlayer(), getYPlayer());
+								motion += 1;
+							}
+						}
+						else if(isUp() && getViewDirect() == ViewDirect.UP) {
+							if(motion > 3)
+								motion = 0;
+							getSsMonster().setXPos((getImgWidth() * motion) + (Gap.COLUMGAP * motion));
+							if(getViewDirect() == ViewDirect.UP) {
+								getSsMonster().setYPos(getImgHeight() * 2 + Gap.ROWGAP * 2);	// 몸 이미지 y좌표
+								getSsMonster().drawObj(getXPlayer(), getYPlayer());
+								motion += 1;
+							}
+						}
+						else if(isRight() && getViewDirect() == ViewDirect.RIGHT) {
+							if(motion > 3)
+								motion = 0;
+							getSsMonster().setXPos((getImgWidth() * motion) + (Gap.COLUMGAP * motion));
+							if(getViewDirect() == ViewDirect.RIGHT) {
+								getSsMonster().setYPos(getImgHeight() * 3 + Gap.ROWGAP * 3);	// 몸 이미지 y좌표
+								getSsMonster().drawObj(getXPlayer(), getYPlayer());
+								motion += 1;
+							}
+						}
+						
+						try {
+							Thread.sleep(100);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						
+					}
+				}
+			}
+		}).start();
+	}
+	
 }
 	
